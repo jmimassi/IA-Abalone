4e30bea344b453a986e4aeef7840205db1da1341
195048 <195048@ecam.be>
2021-04-12 18:28:25 +0200

    Subscribe incomplet

--- abalone-client/client.py
+++ abalone-client/client.py
@@ -1,4 +1,20 @@
-print("hello")
-print("hello")
-print("lalam")
+import socket 
+import json
+import time
 
+import jsonNetwork 
+s = socket.socket()
+s.connect(('127.0.0.1',3000))
+
+def inscription(port):
+	jsonNetwork.sendJSON(s,{
+   "request": "subscribe",
+   "port": port,
+   "name": "fun_name_for_the_client",
+   "matricules": ["12345", "67890"]
+	})
+	recu = jsonNetwork.receiveJSON(s)
+	reponse = str(recu['response'])
+	print(reponse)
+
+inscription(3100)
