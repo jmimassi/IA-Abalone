1bb0134c8909f0f3089a5e29bfaecc9a0b34b78a
195048 <195048@ecam.be>
2021-04-12 19:05:11 +0200

    Debut pong

--- abalone-client/client.py
+++ abalone-client/client.py
@@ -1,6 +1,7 @@
 import socket 
 import json
 import time
+from threading import Thread, Timer
 
 import jsonNetwork 
 s = socket.socket()
@@ -16,5 +17,27 @@ def inscription(port):
 	recu = jsonNetwork.receiveJSON(s)
 	reponse = str(recu['response'])
 	print(reponse)
+	
+
+
+def listenForRequests():
+	def pong():
+		recu = jsonNetwork.receiveJSON(s)
+		print(str(recu['response']))
+		if recu['response'] == 'ping':
+			jsonNetwork.sendJSON(s,{'response':'pong'})
+		else : 
+			print('None')
+	
+	listenThread = Thread(target=pong, daemon=True)
+	listenThread.start()
+
+	def stop():
+		listenThread.join()
+
+	return stop
+
+
 
 inscription(3100)
+
